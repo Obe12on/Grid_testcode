c2764aa3309f6b9aff94f5915a2b6ab73127f1cf
Obe12on <dyuiti.kawata@gmail.com>
2023-02-01 02:45:41 +0900

    Added Stationary check of data

--- 02_SW/CSVloader.py
+++ 02_SW/CSVloader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 class loadCSV:
-        def load(csv_path : str) -> 'pandas.core.frame.DataFrame':
+        def load(csv_path : str) -> 'pd.core.frame.DataFrame':
                 df = pd.read_csv(csv_path)
                 return df
 
--- 02_SW/main.py
+++ 02_SW/main.py
@@ -1,8 +1,15 @@
 import JSONloader
 import CSVloader
 import os
+import FullerTest
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 
 class Main:
+    def __init__(self) -> None:
+        self.isStationary_data = pd.DataFrame()
+
     def readJson(self) -> None:
         cwd = os.getcwd()
         json_path = cwd + "\\" + "config.json"
@@ -13,10 +20,25 @@ class Main:
         self.discount_df = CSVloader.loadCSV.load(self.config_data['Discount_data'])
         self.sales_forecast_df = CSVloader.loadCSV.load(self.config_data['Sales_forecast'])
         self.discount_forecast_df = CSVloader.loadCSV.load(self.config_data['Discount_forecast'])
+
+    def stationaryCheck(self) -> None:
+        productID = self.sales_df.groupby('商品ID')
+        for ID, group in productID:
+            data = group.drop(['商品ID'], axis = 1)
+            data['日付'] = pd.to_datetime(data['日付'], format = '%Y-%m')
+            data.index = data['日付']
+            del data['日付']
+            isStationary = FullerTest.Dickey_Fuller.isStationary(data)
+            self.isStationary_data[ID] = [ isStationary ]
+           
+
     def main(self) -> None:
         self.readJson()
         self.readCSV()
-        print(type(self.sales_df))
+        self.stationaryCheck()
+        print(self.isStationary_data)
+
+        
         
     
 
